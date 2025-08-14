affc886e002a0833d41a9f57bcc8863a58781a6b
Sravan032 <167642797+Sravan032@users.noreply.github.com>
2025-08-14 11:12:28 +0530

    Create bfs.cpp

---
+++ bfs.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void bfs(int start, vector<vector<int>>& adj, int n) {
+    vector<bool> visited(n, false);
+    queue<int> q;
+
+    visited[start] = true;
+    q.push(start);
+
+    while (!q.empty()) {
+        int node = q.front();
+        q.pop();
+        cout << node << " ";
+
+        for (int neighbor : adj[node]) {
+            if (!visited[neighbor]) {
+                visited[neighbor] = true;
+                q.push(neighbor);
+            }
+        }
+    }
+}
+
+int main() {
+    int n = 5; // number of nodes
+    vector<vector<int>> adj(n);
+
+    // directly adding edges
+    adj[0] = {1, 2};
+    adj[1] = {0, 3};
+    adj[2] = {0, 4};
+    adj[3] = {1};
+    adj[4] = {2};
+
+    cout << "BFS starting from node 0: ";
+    bfs(0, adj, n);
+    cout << endl;
+
+    return 0;
+}
